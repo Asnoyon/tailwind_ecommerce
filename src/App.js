32d58e88242adac54c1274c9c3c264cae4a401d4
Asnoyon <blackpain1100@gmail.com>
2023-08-27 01:22:44 +0600

    App js update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/header/Index";
+import Home from "./modules/home/Index";
 
+import { Routes, Route } from "react-router-dom";
+import Product from "./modules/product/Index";
+import Footer from "./components/footer/Index";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:id" element={<Product />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
