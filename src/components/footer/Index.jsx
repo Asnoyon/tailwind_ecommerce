8e908904e62745e26c17f11430a803c220a5569b
Asnoyon <blackpain1100@gmail.com>
2023-09-08 23:54:31 +0600

    Index update

--- src/components/footer/Index.jsx
+++ src/components/footer/Index.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import YouTubeIcon from '@material-ui/icons/YouTube';
 const Footer = () => {
   return (
     <footer className="text-gray-600 body-font border-t-2">
@@ -18,86 +21,109 @@ const Footer = () => {
             >
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
-            <span className="ml-3 text-xl">Tailblocks</span>
+            <span className="ml-3 text-xl">E-commerce</span>
           </a>
-          <p className="mt-2 text-sm text-gray-500">
-            Air plant banjo lyft occupy retro adaptogen indego
-          </p>
+          <h2 className="mt-2 font-medium text-gray-900">
+            Sign up & get 10% off your next order
+          </h2>
+            <div class=" mb-3">
+              <input class="mr-3 my-3 p-2 shadow-inner text-white rounded px-3 text-sm bg-white-900  border-gray-700" placeholder="Enter your email" type="text"/>
+              <button className="shadow-lg bg-indigo-500 text-white font-bold text-sm rounded px-3 py-2 mb-3">Subscribe</button>
+          </div>
+          <div className="">
+              <FacebookIcon className="mr-1"></FacebookIcon>
+              <InstagramIcon className="mr-1"></InstagramIcon>
+              <TwitterIcon className="mr-1"></TwitterIcon>
+              <YouTubeIcon className="mr-1"></YouTubeIcon>
+          </div>
         </div>
         <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
           <div className="lg:w-1/4 md:w-1/2 w-full px-4">
             <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-              CATEGORIES
+              Customer Care
             </h2>
             <nav className="list-none mb-10">
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">First Link</a>
+              <li className="pb-1">
+                <a className="text-gray-600 hover:text-gray-800 ">Contact Us</a>
               </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+              <li className="pb-1">
+                <a className="text-gray-600 hover:text-gray-800">FAQs</a>
               </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Third Link</a>
+              <li className="pb-1">
+                <a className="text-gray-600 hover:text-gray-800">Return & Exchanges</a>
               </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+              <li className="pb-1">
+                <a className="text-gray-600 hover:text-gray-800">Shipping and Handling</a>
+              </li>
+              <li className="pb-1">
+                <a className="text-gray-600 hover:text-gray-800">Damaged or Defected Items</a>
+              </li>
+              <li className="pb-1">
+                <a className="text-gray-600 hover:text-gray-800">Canceling & Changing an Order</a>
+              </li>
+              <li className="pb-1">
+                <a className="text-gray-600 hover:text-gray-800">Terms of Service</a>
+              </li>
+              <li className="pb-1">
+                <a className="text-gray-600 hover:text-gray-800">Privacy Policy</a>
               </li>
             </nav>
           </div>
           <div className="lg:w-1/4 md:w-1/2 w-full px-4">
             <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-              CATEGORIES
+              Inside Grace Eleyae
             </h2>
             <nav className="list-none mb-10">
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">First Link</a>
+              <li className="pb-1">
+                <a className="text-gray-600 hover:text-gray-800 ">About Us</a>
               </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+              <li className="pb-1">
+                <a className="text-gray-600 hover:text-gray-800">GE Magazine</a>
               </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Third Link</a>
+              <li className="pb-1">
+                <a className="text-gray-600 hover:text-gray-800">Contact Us</a>
               </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+              <li className="pb-1">
+                <a className="text-gray-600 hover:text-gray-800">Wholesale</a>
+              </li>
+              <li className="pb-1">
+                <a className="text-gray-600 hover:text-gray-800">Careers</a>
+              </li>
+              
+            </nav>
+          </div>
+          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+              My Account
+            </h2>
+            <nav className="list-none mb-10">
+              <li className="pb-1">
+                <a className="text-gray-600 hover:text-gray-800 ">Sign In/Register</a>
+              </li>
+              <li className="pb-1">
+                <a className="text-gray-600 hover:text-gray-800">My Wishlist</a>
+              </li>
+              <li className="pb-1">
+                <a className="text-gray-600 hover:text-gray-800">My Cart</a>
               </li>
             </nav>
           </div>
           <div className="lg:w-1/4 md:w-1/2 w-full px-4">
             <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-              CATEGORIES
+              Top Searches
             </h2>
             <nav className="list-none mb-10">
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">First Link</a>
+              <li className="mb-1">
+                <a className="text-gray-600 hover:text-gray-800">Socklists</a>
               </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+              <li className="mb-1">
+                <a className="text-gray-600 hover:text-gray-800">Shipping</a>
               </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Third Link</a>
+              <li className="mb-1">
+                <a className="text-gray-600 hover:text-gray-800">Black</a>
               </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
-              </li>
-            </nav>
-          </div>
-          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-              CATEGORIES
-            </h2>
-            <nav className="list-none mb-10">
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">First Link</a>
-              </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Second Link</a>
-              </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Third Link</a>
-              </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+              <li className="mb-1">
+                <a className="text-gray-600 hover:text-gray-800">Sizing</a>
               </li>
             </nav>
           </div>
