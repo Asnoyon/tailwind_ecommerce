b935836f7d0fa2e04a0457727511622e13d93292
Asnoyon <blackpain1100@gmail.com>
2023-09-09 10:23:48 +0600

    App js update

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ import Home from "./modules/home/Index";
 import { Routes, Route } from "react-router-dom";
 import Product from "./modules/product/Index";
 import Footer from "./components/footer/Index";
+import Products from "./modules/products";
 
 function App() {
   return (
     <div className="bg-slate-100">
-      <Header />
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products/:id" element={<Product />} />
+        <Route path="/products" element={<Products/>}/>
+        <Route path="*" element={<div>404</div>}/>
       </Routes>
       <Footer />
     </div>
